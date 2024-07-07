b4ab3927f21bfef68411148e0b743108c3720b6e
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-07 21:47:31 +0300

    Create OnBoarding Screen

--- lib/features/on_boarding/presentation/screens/onboarding_screen.dart
+++ lib/features/on_boarding/presentation/screens/onboarding_screen.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+import '../widgets/on_boarding_page_view.dart';
+
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
+
   static const routeName = 'onBoarding';
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  late PageController pageController;
+
+  var currentPage = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('OnBoarding Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: OnBoardingPageView(
+                pageController: pageController,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
