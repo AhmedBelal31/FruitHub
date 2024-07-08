d4dcfa5ac49e3a2c51f7b12ab67d4e63682b223a
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-08 22:53:16 +0300

    Localization files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruit_hub/generated/l10n.dart';
 
 import 'features/splash/presentation/screens/splash_screen.dart';
 
@@ -13,20 +15,28 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FruitHub',
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
       theme: ThemeData(
           // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           // useMaterial3: true,
           scaffoldBackgroundColor: Colors.white,
           appBarTheme: const AppBarTheme(
-            scrolledUnderElevation: 0,
-           color: Colors.white ,
-           titleTextStyle: TextStyle(color: Colors.black)
-    
-          )),
+              scrolledUnderElevation: 0,
+              color: Colors.white,
+              titleTextStyle: TextStyle(color: Colors.black))),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashScreen.routeName,
+
+
     );
   }
 }
