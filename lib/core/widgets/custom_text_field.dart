4fbbed3df5aefed694290ab49ef47877533ce67d
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-11 23:11:54 +0300

    Create Custom Text Field

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../utils/app_colors.dart';
+import '../utils/app_text_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+
+  final void Function(String)? onFieldSubmitted;
+
+  final TextEditingController? controller;
+
+  final TextInputType? keyboardType;
+  final bool? obscureText;
+  final Widget? suffixIcon;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.validator,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.controller,
+    this.keyboardType,
+    this.obscureText,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      onChanged: onChanged,
+      onFieldSubmitted: onFieldSubmitted,
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText ?? false,
+      cursorColor: AppColors.kButtonPrimaryColor,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyles.bold13.copyWith(
+          color: const Color(0xFF949D9E),
+        ),
+        suffixIcon: suffixIcon,
+        border: buildOutlineInputBorder(),
+        enabledBorder: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+        fillColor: const Color(0xFFF9FAFA),
+        filled: true,
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: const BorderSide(
+          color: Color(0xFFE6E9E9),
+          width: 1.0,
+        ));
+  }
+}
