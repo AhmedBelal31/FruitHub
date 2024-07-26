686405284b54f234a07a5c196ad0a90ed59b0ad2
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-26 12:11:39 +0300

    Build Don't Have An Account Widget

--- lib/core/widgets/do_not_have_an_account.dart
+++ lib/core/widgets/do_not_have_an_account.dart
@@ -1,44 +1,50 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../utils/app_colors.dart';
 import '../utils/app_text_styles.dart';
 
 class DoNotHaveAnAccount extends StatelessWidget {
-  final Function()? onTap ;
+  final Function()? onTap;
+
   const DoNotHaveAnAccount({super.key, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    // return GestureDetector(
-    //   onTap: onTap,
-    //   child: Text.rich(TextSpan(children: [
-    //     TextSpan(
-    //       text: 'ليس لديك حساب؟ ',
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(
+            text: 'ليس لديك حساب؟ ',
+            style:
+                TextStyles.semiBold16.copyWith(color: const Color(0xFF949D9E)),
+          ),
+          TextSpan(
+            text: 'قم بإنشاء حساب',
+            recognizer: TapGestureRecognizer()..onTap = onTap,
+            style:
+                TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),
+          ),
+        ],
+      ),
+    );
+
+    // return Row(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   children: [
+    //     Text(
+    //       'ليس لديك حساب؟ ',
     //       style: TextStyles.semiBold16.copyWith(color: const Color(0xFF949D9E)),
     //     ),
-    //     TextSpan(
-    //       text: 'قم بإنشاء حساب',
-    //       style: TextStyles.semiBold16.copyWith(color:  AppColors.primaryColor),
+    //
+    //     GestureDetector(
+    //       onTap: onTap,
+    //       child: Text(
+    //         'قم بإنشاء حساب',
+    //         style: TextStyles.semiBold16.copyWith(color:  AppColors.primaryColor),
+    //       ),
     //     ),
-    //   ])),
+    //
+    //   ],
     // );
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'ليس لديك حساب؟ ',
-          style: TextStyles.semiBold16.copyWith(color: const Color(0xFF949D9E)),
-        ),
-
-        GestureDetector(
-          onTap: onTap,
-          child: Text(
-            'قم بإنشاء حساب',
-            style: TextStyles.semiBold16.copyWith(color:  AppColors.primaryColor),
-          ),
-        ),
-
-      ],
-    );
   }
 }
