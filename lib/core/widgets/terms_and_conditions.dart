59b38a1919a835edcb5c98e759f408952ff3a08f
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-28 22:06:21 +0300

    Create Terms And Conditions

---
+++ lib/core/widgets/terms_and_conditions.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../constants.dart';
+import '../utils/app_colors.dart';
+import '../utils/app_images.dart';
+import '../utils/app_text_styles.dart';
+
+class TermsAndConditions extends StatefulWidget {
+  const TermsAndConditions({
+    super.key,
+  });
+
+  @override
+  State<TermsAndConditions> createState() => _TermsAndConditionsState();
+}
+
+class _TermsAndConditionsState extends State<TermsAndConditions> {
+  bool isTermsAccepted = false  ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CustomCheckBox(
+          isChecked: isTermsAccepted,
+          onChecked: (value)
+          {
+            setState(() {
+             isTermsAccepted = value;
+
+            });
+          },
+        ),
+        const SizedBox(width: 16),
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width -
+              (kDefaultPadding * 2) -
+              24 -
+              16,
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'من خلال إنشاء حساب ، فإنك توافق على ',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: const Color(0xFF949D9E),
+                  ),
+                ),
+                TextSpan(
+                  text: 'الشروط والأحكام الخاصة بنا ',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Function(bool) onChecked;
+  const CustomCheckBox({super.key, required this.isChecked, required this.onChecked});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()
+      {
+        onChecked(!isChecked);
+      },
+      child: AnimatedContainer(
+        width: 24,
+        height: 24,
+        duration: const Duration(
+          milliseconds: 200,
+        ),
+        decoration: ShapeDecoration(
+          color: isChecked ? AppColors.primaryColor : Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+            side: BorderSide(
+              width: 1.5,
+              color: isChecked ? Colors.transparent : const Color(0xFFDCDEDE),
+            ),
+          ),
+        ),
+        child: isChecked ?  Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: SvgPicture.asset(
+            Assets.assetsImagesCheck ,
+
+          ),
+        ) : const SizedBox(),
+      ),
+    );
+  }
+}
