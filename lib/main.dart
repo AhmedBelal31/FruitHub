fe4b4f2f3c07a9666fb71ccfc0542e353c9b3560
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-07-29 21:21:25 +0300

    Add Firebase To Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper_functions/on_generate_routes.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
-import 'package:fruit_hub/core/utils/app_strings.dart';
-import 'package:fruit_hub/features/home/presentation/screens/home_screen.dart';
 import 'package:fruit_hub/generated/l10n.dart';
-
 import 'core/services/app_pref.dart';
 import 'core/utils/app_text_styles.dart';
 import 'features/splash/presentation/screens/splash_screen.dart';
-
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const FruitHub());
 }
