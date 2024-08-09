39bb4e539319bd85faaae8563612459931ab4d5c
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-08-09 06:51:20 +0300

    Register Cubits

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -3,6 +3,7 @@ import 'package:fruit_hub/features/login/presentation/cubits/sign_in_cubit/sign_
 import 'package:get_it/get_it.dart';
 
 import '../../features/login/data/repos/auth_repo_impl.dart';
+import '../../features/login/presentation/cubits/sign_up_cubit/sign_up_cubit.dart';
 import 'firebase_auth_service.dart';
 
 class ServiceLocator {
@@ -16,10 +17,10 @@ class ServiceLocator {
     getIt.registerLazySingleton<AuthRepo>(
       () => AuthRepoImpl(firebaseAuthService: getIt<FirebaseAuthService>()),
     );
+    //Register SignUpCubit
+    getIt.registerFactory<SignUpCubit>(() => SignUpCubit(authRepo: getIt<AuthRepo>()));
 
-    //getIt.registerFactory<SignUpCubit>(() => SignUpCubit(authRepo: getIt<AuthRepo>()));
-
+    //Register SignInCubit
     getIt.registerFactory<SignInCubit>(() => SignInCubit(authRepo: getIt<AuthRepo>()));
-
   }
 }
